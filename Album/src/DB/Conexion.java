41ee7110ea3906908b5e8f9945fed634d6b3af09
dchacon2404 <dchacon2404@gmail.com>
2022-07-17 11:55:02 -0600

    En la clase conexion se importaron las librerias connection, driverManager y statement

--- Album/src/DB/Conexion.java
+++ Album/src/DB/Conexion.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package DB;
 
-/**
- *
- * @author alelo
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class Conexion {
     
+   
 }
