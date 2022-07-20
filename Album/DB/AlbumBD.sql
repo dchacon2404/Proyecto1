7273e5c0a70d51bafdf0516dbcb76cd4b1fff274
FuentesC <af93511@gmail.com>
2022-07-20 11:05:01 -0600

    Error en el pull #24, ahora si se actualizó la BD con los cambios

--- Album/DB/AlbumBD.sql
+++ Album/DB/AlbumBD.sql
@@ -32,8 +32,18 @@ CREATE TABLE `perfil` (
   `NombreUsuario` varchar(45) DEFAULT NULL,
   `Contrasena` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idPerfil`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `perfil`
+--
+
+LOCK TABLES `perfil` WRITE;
+/*!40000 ALTER TABLE `perfil` DISABLE KEYS */;
+INSERT INTO `perfil` VALUES (1,'Andrés','Fuentes Corella',19,'andrescr','0000');
+/*!40000 ALTER TABLE `perfil` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `personajes`
@@ -44,11 +54,21 @@ DROP TABLE IF EXISTS `personajes`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `personajes` (
   `idPersonaje` int NOT NULL,
-  `imagen` varchar(100) DEFAULT NULL,
+  `imagen` longblob,
   `bueno` tinyint DEFAULT NULL,
   PRIMARY KEY (`idPersonaje`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `personajes`
+--
+
+LOCK TABLES `personajes` WRITE;
+/*!40000 ALTER TABLE `personajes` DISABLE KEYS */;
+INSERT INTO `personajes` VALUES (1,_binary 'C:UsersAndresOneDriveEscritorioGITHUBProyecto1AlbumSOBRESBuenosAnakinSky',1),(2,_binary 'C:UsersAndresOneDriveEscritorioGITHUBProyecto1AlbumSOBRESBuenosC3PO',1),(3,_binary 'C:UsersAndresOneDriveEscritorioGITHUBProyecto1AlbumSOBRESBuenosChewbacca',1),(4,_binary 'C:UsersAndresOneDriveEscritorioGITHUBProyecto1AlbumSOBRESBuenosHanSolo',1),(5,_binary 'C:UsersAndresOneDriveEscritorioGITHUBProyecto1AlbumSOBRESBuenosLukeNiño',1),(6,_binary 'C:UsersAndresOneDriveEscritorioGITHUBProyecto1AlbumSOBRESBuenosObiWanK',1),(7,_binary 'C:UsersAndresOneDriveEscritorioGITHUBProyecto1AlbumSOBRESBuenosPrincesaLeia',1),(8,_binary 'C:UsersAndresOneDriveEscritorioGITHUBProyecto1AlbumSOBRESBuenosquin-gon',1),(9,_binary 'C:UsersAndresOneDriveEscritorioGITHUBProyecto1AlbumSOBRESBuenosR2-D2',1),(10,_binary 'C:UsersAndresOneDriveEscritorioGITHUBProyecto1AlbumSOBRESBuenosYoda',1),(11,_binary 'C:UsersAndresOneDriveEscritorioGITHUBProyecto1AlbumSOBRESMalosBattleDroid',0),(12,_binary 'C:UsersAndresOneDriveEscritorioGITHUBProyecto1AlbumSOBRESMalosCapitanPhasma',0),(13,_binary 'C:UsersAndresOneDriveEscritorioGITHUBProyecto1AlbumSOBRESMalosCapitanRex',0),(14,_binary 'C:UsersAndresOneDriveEscritorioGITHUBProyecto1AlbumSOBRESMalosComandanteBacara',0),(15,_binary 'C:UsersAndresOneDriveEscritorioGITHUBProyecto1AlbumSOBRESMalosDroideka',0),(16,_binary 'C:UsersAndresOneDriveEscritorioGITHUBProyecto1AlbumSOBRESMalosKylo',0),(17,_binary 'C:UsersAndresOneDriveEscritorioGITHUBProyecto1AlbumSOBRESMalosSandTrooper',0),(18,_binary 'C:UsersAndresOneDriveEscritorioGITHUBProyecto1AlbumSOBRESMalosScoutTrooper',0),(19,_binary 'C:UsersAndresOneDriveEscritorioGITHUBProyecto1AlbumSOBRESMalosSnowTrooper',0),(20,_binary 'C:UsersAndresOneDriveEscritorioGITHUBProyecto1AlbumSOBRESMalosStormTrooper',0);
+/*!40000 ALTER TABLE `personajes` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `postales`
@@ -61,7 +81,7 @@ CREATE TABLE `postales` (
   `idPostal` int NOT NULL AUTO_INCREMENT,
   `idPerfil` int DEFAULT NULL,
   `idPersonaje` int DEFAULT NULL,
-  `imagen` varchar(100) DEFAULT NULL,
+  `imagen` longblob,
   PRIMARY KEY (`idPostal`),
   KEY `idPerfil_idx` (`idPerfil`),
   KEY `idPersonaje_idx` (`idPersonaje`),
@@ -69,6 +89,15 @@ CREATE TABLE `postales` (
   CONSTRAINT `idPersonaje` FOREIGN KEY (`idPersonaje`) REFERENCES `personajes` (`idPersonaje`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `postales`
+--
+
+LOCK TABLES `postales` WRITE;
+/*!40000 ALTER TABLE `postales` DISABLE KEYS */;
+/*!40000 ALTER TABLE `postales` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -79,4 +108,4 @@ CREATE TABLE `postales` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-07-18 13:34:58
+-- Dump completed on 2022-07-20 11:02:07
